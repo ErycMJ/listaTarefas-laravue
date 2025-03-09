38812fd8cc90c434835456fc22be2916a3e3de54
Eryc Jacinto <erycmj@gmail.com>
2025-03-09 00:53:25 -0300

    First commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,10 @@
 <template>
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-            <TodoSpinner v-if="loading"/>
-
-            <template v-else>
-                <TodoFormAdd/>
-                <TodoItens v-if="$store.state.todos.length"/>
-                <TodoEmpty v-else/>
-            </template>
+            <TodoSpinner/>
+            <TodoFormAdd/>
+            <TodoItens/>
+            <TodoEmpty/>
         </div>
     </div>
 </template>
@@ -25,17 +22,6 @@ export default {
     TodoFormAdd,
     TodoItens,
     TodoEmpty
-  },
-  data() {
-    return {
-      loading: false
-    }
-  },
-  created() {    
-    this.loading = true;
-    this.$store.dispatch('getTodos').finally(() => {
-      this.loading = false;
-    });
   }
 }
 </script>
---
+++ src/components/TodoItens.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem 
+            v-for="todos in $store.state.todos"
+            :key="todos.id"
+            :todo="todos"
+        />
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    components: { TodoItem }
+}
+</script>
